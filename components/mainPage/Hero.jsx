065c731694ba4c9e8d2cdeafb000b6eaa1f5a0f9
Mohith kumar <murugan28aug@gmail.com>
2023-01-15 21:21:58 +0530

    link

--- components/mainPage/Hero.jsx
+++ components/mainPage/Hero.jsx
@@ -1,5 +1,6 @@
 import Chat from "./Chat"
 import { motion} from "framer-motion"
+import Link from "next/link"
  
 const Hero = () => {
     return (
@@ -27,7 +28,7 @@ const Hero = () => {
         <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
       </svg>
       
-            Mail Us</motion.button>
+         <Link href="mailto:joancampeny5@gmail.com">  Mail Us</Link> </motion.button>
         {/* <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button> */}
       </div>
     </motion.div>
